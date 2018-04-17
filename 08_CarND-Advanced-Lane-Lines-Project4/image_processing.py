15bf1131d528cbfd6ea3176852103f69349fa710
lb5160482 <lbo2@jhu.edu>
2018-04-16 23:35:59 -0700

    P04: perspective transformation

--- 08_CarND-Advanced-Lane-Lines-Project4/image_processing.py
+++ 08_CarND-Advanced-Lane-Lines-Project4/image_processing.py
@@ -3,46 +3,58 @@ import numpy as np
 
 
 def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	if (orient == 'x'):
-		sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
-	else:
-		sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
-	sobel_absolute = np.absolute(sobel)
-	scaled = np.uint8(255 * sobel_absolute / np.max(sobel_absolute))
-	binary = np.zeros_like(scaled)
-	binary[(scaled >= thresh_min) & (scaled <= thresh_max)] = 255
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    if (orient == 'x'):
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+    else:
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+    sobel_absolute = np.absolute(sobel)
+    scaled = np.uint8(255 * sobel_absolute / np.max(sobel_absolute))
+    binary = np.zeros_like(scaled)
+    binary[(scaled >= thresh_min) & (scaled <= thresh_max)] = 255
 
-	return binary
+    return binary
 
 
 def mag_thresh(img, sobel_kernel=3, thresh=(0, 255)):
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-	sobel_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-	sobel_mag = np.sqrt(np.multiply(sobel_x, sobel_x) + np.multiply(sobel_y, sobel_y))
-	scaled_mag = 255 * sobel_mag / np.max(sobel_mag)
-	binary_output = np.zeros_like(scaled_mag)
-	binary_output[(scaled_mag > thresh[0]) & (scaled_mag < thresh[1])] = 255
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobel_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    sobel_mag = np.sqrt(np.multiply(sobel_x, sobel_x) + np.multiply(sobel_y, sobel_y))
+    scaled_mag = 255 * sobel_mag / np.max(sobel_mag)
+    binary_output = np.zeros_like(scaled_mag)
+    binary_output[(scaled_mag > thresh[0]) & (scaled_mag < thresh[1])] = 255
 
-	return binary_output
+    return binary_output
 
 
 def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi / 2)):
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	sobel_x = np.abs(cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
-	sobel_y = np.abs(cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
-	direction = np.arctan2(sobel_y, sobel_x)
-	threshed_direction = np.zeros_like(direction)
-	threshed_direction[(direction > thresh[0]) & (direction < thresh[1])] = 255
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sobel_x = np.abs(cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
+    sobel_y = np.abs(cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
+    direction = np.arctan2(sobel_y, sobel_x)
+    threshed_direction = np.zeros_like(direction)
+    threshed_direction[(direction > thresh[0]) & (direction < thresh[1])] = 255
 
-	return threshed_direction
+    return threshed_direction
 
 
 def hls_s_threshol(img, thresh=(0, 255)):
-	hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-	s_channel = hls[:, :, 2]
-	binary = np.zeros_like(s_channel)
-	binary[(s_channel > thresh[0]) & (s_channel <= thresh[1])] = 255
+    hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+    s_channel = hls[:, :, 2]
+    binary = np.zeros_like(s_channel)
+    binary[(s_channel > thresh[0]) & (s_channel <= thresh[1])] = 255
 
-	return binary
+    return binary
+
+
+def perspective_transfrom(img, src_points, dst_points):
+    img_size = img.shape[1::-1]
+    transformation = cv2.getPerspectiveTransform(src_points, dst_points)
+    perspective_transformed = cv2.warpPerspective(img, transformation, img_size)
+    # Minv = cv2.getPerspectiveTransform(dst_points, src_points)
+    # aa = cv2.warpPerspective(perspective_transformed, Minv, img_size)
+    # cv2.imshow('aa', aa)
+    # cv2.waitKey(0)
+    return perspective_transformed
+
--- 08_CarND-Advanced-Lane-Lines-Project4/main.py
+++ 08_CarND-Advanced-Lane-Lines-Project4/main.py
@@ -4,6 +4,10 @@ from camera_calibration import CameraCalibration
 import image_processing as imgproc
 import numpy as np
 
+image_size = (1280, 720)
+bin_img_dict = {}
+
+
 # Part 1 -- Camera calibration
 calibration = CameraCalibration('camera_cal/')
 calibration.calibrate()
@@ -15,36 +19,77 @@ print('Camera distortion parameters are :')
 print(dist_paras)
 print()
 
+
 # Part 2 -- Apply a distortion correction to raw images.
 calibration.undistort_images()
 print()
 
+
 # Part 3 -- Use color transforms, gradients, etc., to create a thresholded binary image
 test_image_paths = glob.glob('./test_images/*.jpg')
 print('Start generating binary images from sample images...')
-bin_img_dict = {}
 for test_img_path in test_image_paths:
-	test_img = cv2.imread(test_img_path)
-	# test_img = cv2.GaussianBlur(test_img, (5, 5), 0)
-	kernel = np.ones((3, 3), np.uint8)
-	test_img = cv2.erode(test_img, kernel, iterations=1)
-	test_img = cv2.dilate(test_img, kernel, iterations=1)
+    test_img = cv2.imread(test_img_path)
+    # distortion correction
+    test_img = calibration.distort_correction(test_img)
+    # test_img = cv2.GaussianBlur(test_img, (5, 5), 0)
+    kernel = np.ones((3, 3), np.uint8)
+    test_img = cv2.erode(test_img, kernel, iterations=1)
+    test_img = cv2.dilate(test_img, kernel, iterations=1)
 
-	# binary images from different processing methods
-	sobel_x_bin = imgproc.abs_sobel_thresh(test_img, orient='x',thresh_min=20, thresh_max=100)
-	sobel_y_bin = imgproc.abs_sobel_thresh(test_img, orient='y', thresh_min=20, thresh_max=100)
-	mag_bin = imgproc.mag_thresh(test_img, sobel_kernel=5, thresh=(30, 100))
-	dir_bin = imgproc.dir_threshold(test_img, sobel_kernel=5, thresh=(0.7, 1.3))
-	s_channel_bin = imgproc.hls_s_threshol(test_img, thresh=(160, 255))
+    # binary images from different processing methods
+    sobel_x_bin = imgproc.abs_sobel_thresh(test_img, orient='x', thresh_min=20, thresh_max=100)
+    sobel_y_bin = imgproc.abs_sobel_thresh(test_img, orient='y', thresh_min=20, thresh_max=100)
+    mag_bin = imgproc.mag_thresh(test_img, sobel_kernel=5, thresh=(30, 100))
+    dir_bin = imgproc.dir_threshold(test_img, sobel_kernel=5, thresh=(0.7, 1.3))
+    s_channel_bin = imgproc.hls_s_threshol(test_img, thresh=(160, 255))
 
-	# combination of binary images
-	combine = np.zeros_like(test_img[:, :, 0])
-	combine[((sobel_x_bin == 255) & (sobel_y_bin == 255)) | ((mag_bin == 255) & (dir_bin == 255)) | (
-			s_channel_bin == 255)] = 255
-	window_name = test_img_path[test_img_path.rfind('\\') + 1:]
-	bin_img_dict[window_name] = combine
+    # combination of binary images
+    combine = np.zeros_like(test_img[:, :, 0])
+    combine[((sobel_x_bin == 255) & (sobel_y_bin == 255)) | ((mag_bin == 255) & (dir_bin == 255)) | (
+            s_channel_bin == 255)] = 255
 
-	# file_name = './output_images/bin_' + window_name;
-	# cv2.imwrite(file_name, combine)
+    window_name = test_img_path[test_img_path.rfind('/') + 1:]
+    # cv2.imshow(window_name, combine)
+    # cv2.waitKey(0)
+    # cv2.destroyWindow(window_name)
+
+    # file_name = './output_images/bin_' + window_name;
+    # cv2.imwrite(file_name, combine)
+
+    bin_img_dict[window_name] = combine
 print('Finish generating binary images from sample images!')
 print()
+
+
+# Part 4 -- Apply a perspective transform to rectify binary image ("birds-eye view")
+print('Start perspective transformation...')
+offset = 200
+perspective_src_points = np.float32([[233, 694], [595, 450], [686, 450], [1073, 694]])  # These points are manually selected
+perspective_dst_points = np.float32([[offset, image_size[1]], [offset, 0],
+                                     [image_size[0] - offset, 0], [image_size[0] - offset, image_size[1]]])
+for test_img_path in test_image_paths:
+    img_name = test_img_path[test_img_path.rfind('/') + 1:]
+    test_img = cv2.imread(test_img_path)
+    # distortion correction
+    test_img = calibration.distort_correction(test_img)
+
+    # file_name = './output_images/undist_' + img_name;
+    # cv2.imwrite(file_name, test_img)
+
+    cv2.line(test_img, (233, 694), (595, 450), color=(0, 0, 255))
+    cv2.line(test_img, (686, 450), (1073, 694), color=(0, 0, 255))
+    # file_name = './output_images/perepective_region.jpg'
+    # cv2.imwrite(file_name, test_img)
+    bird_view_img_rgb = imgproc.perspective_transfrom(test_img, perspective_src_points, perspective_dst_points)
+    bird_view_img_binary = imgproc.perspective_transfrom(bin_img_dict[img_name], perspective_src_points, perspective_dst_points)
+
+    # cv2.imshow(img_name, bird_view_img_rgb)
+    # cv2.imshow(img_name, bird_view_img_binary)
+    # cv2.waitKey(0)
+    # cv2.destroyWindow(window_name)
+
+    cv2.imwrite('./output_images/bird_view_rgb_' + img_name, bird_view_img_rgb)
+    cv2.imwrite('./output_images/bird_view_binary_' + img_name, bird_view_img_binary)
+
+print('Perpective transformation finished!')
