d48e1a5e4d7b246dfff79549eec2828db376f7b6
lb5160482 <lbo2@jhu.edu>
2018-05-01 23:38:23 -0700

    Project5: All finished, raw

--- 09_CarND-Vehicle-Detection_Project5/main.py
+++ 09_CarND-Vehicle-Detection_Project5/main.py
@@ -4,7 +4,7 @@ import os
 import pickle
 
 
-cap = cv2.VideoCapture('./project_video_clip.mp4')
+cap = cv2.VideoCapture('./project_video.mp4')
 
 file_name = 'model_dict'
 if os.path.exists(file_name):
